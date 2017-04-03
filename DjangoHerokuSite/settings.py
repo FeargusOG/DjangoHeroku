7792cc807215d2482a21251ec4bf29992cd8606f
Feargus Lenovo VM <feargusogorman@gmail.com>
2017-04-03 15:57:02 +0100

    Test push from VM

--- DjangoHerokuSite/settings.py
+++ DjangoHerokuSite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
+#Test git push from VM
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
