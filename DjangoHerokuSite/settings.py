cfb67a2258062e5d4b9f8466c4d51e7b1cfc2043
Feargus O'Gorman <feargusogorman@gmail.com>
2017-05-22 00:07:38 +0100

    Added logic for adding game and price

--- DjangoHerokuSite/settings.py
+++ DjangoHerokuSite/settings.py
@@ -18,7 +18,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -126,7 +125,8 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
--- psnvalue/library_manager.py
+++ psnvalue/library_manager.py
@@ -1,3 +1,156 @@
+import json
+import requests
+import collections
 
-def update_library(library_id):
-    print("Gonna update the lib: ", library_id)
+from django.utils import timezone
+from .models import Library, GameList, GamePrice
+
+#PSN Each Game JSON element
+PSN_JSON_ELEM_EACH_GAME = 'links'
+#PSN Sub-Base Game JSON element i.e. bundles etc.
+PSN_JSON_ELEM_SUB_GAME = 'parent_name'
+#PSN TODO Maybe use this for identifying full games
+PSN_JSON_ELEM_FULL_GAME = 'Full Game'
+#PSN Game ID JSON element
+PSN_JSON_ELEM_GAME_ID = 'id'
+#PSN Game Name JSON element
+PSN_JSON_ELEM_GAME_NAME = 'name'
+#PSN Game Full Detials URL JSON element
+PSN_JSON_ELEM_GAME_URL = 'url'
+#PSN Game Platform List JSON element
+PSN_JSON_ELEM_GAME_PFORMS = 'playable_platform'
+#PSN Game PS4 Platform Title
+PSN_JSON_ELEM_GAME_PS4_PFORM = 'PS4™'
+#PSN Game Image list JSON element
+PSN_JSON_ELEM_GAME_IMAGES = 'images'
+#PSN Game Image type JSON element
+PSN_JSON_ELEM_GAME_IMGTYPE = 'type'
+#PSN Game Image thumb type
+PSN_JSON_ELEM_GAME_IMGTYPE_THUMB = 1
+#PSN Game Age Rating JSON element - part of the full game details json!
+PSN_JSON_ELEM_GAME_AGERATING = 'age_limit'
+#PSN Default Age Rating
+PSN_DEFAULT_AGE_RATING = 0
+#PSN Main Game details block
+PSN_JSON_ELEM_GAME_PRICE_BLOCK = 'default_sku'
+#PSN Base Price JSON element - part of the full game details json!
+PSN_JSON_ELEM_GAME_BASE_PRICE = 'price'
+#PSN Base Discount JSON element - part of the full game details json!
+PSN_JSON_ELEM_GAME_REWARDS = 'rewards'
+#PSN Base Discount JSON element - part of the full game details json!
+PSN_JSON_ELEM_GAME_BASE_DISCOUNT = 'discount'
+#PSN PS Plus Discount JSON element - part of the full game details json!
+PSN_JSON_ELEM_GAME_BONUS_DISCOUNT = 'bonus_discount'
+
+
+def update_library(p_library_id):
+    print("Gonna update the lib: ", p_library_id)
+    #For the moment, only psn is supported.
+    update_psn_lib(p_library_id)
+
+def update_psn_lib(p_library_id):
+    count_of_base_games = 0
+    psn_lib_json = get_psn_lib_json()
+
+    for eachGame in psn_lib_json[PSN_JSON_ELEM_EACH_GAME]:
+        # We're just looking for base games i.e. ignore bundles etc.
+        # If a game is on sale, the bundle is (always?) on sale too.
+        # Maybe we shouldn't be.... Do some tests on this.
+        if PSN_JSON_ELEM_SUB_GAME in eachGame:
+            continue
+
+        # We found a base game, so parse the details
+        count_of_base_games += 1
+        if(game_exists_in_db(p_library_id, eachGame[PSN_JSON_ELEM_GAME_ID])):
+            print("Game exists")
+        else:
+            print("Game doesn't exist")
+            add_psn_game(p_library_id, eachGame)
+            break
+
+def add_psn_game(p_library_id, p_base_game_json):
+    g_id = p_base_game_json[PSN_JSON_ELEM_GAME_ID]
+    g_name = p_base_game_json[PSN_JSON_ELEM_GAME_NAME]
+    g_url = p_base_game_json[PSN_JSON_ELEM_GAME_URL]
+    g_thumb = get_psn_thumbnail(p_base_game_json[PSN_JSON_ELEM_GAME_IMAGES])
+    g_age = PSN_DEFAULT_AGE_RATING
+    print("Adding game: ", g_name, " - ", g_url)
+    game_entry = add_game_list_entry_to_db(g_id, g_name, g_url, g_thumb, g_age, Library.objects.get(pk=p_library_id))
+    add_psn_full_game_details(game_entry)
+
+def add_psn_full_game_details(p_game_entry):
+    full_details_json = get_psn_game_json(p_game_entry.json_url)
+    print("Game: ",full_details_json[PSN_JSON_ELEM_GAME_NAME],"- Age: ",full_details_json[PSN_JSON_ELEM_GAME_AGERATING])
+    #Update the age rating
+    g_age = full_details_json[PSN_JSON_ELEM_GAME_AGERATING]
+    set_game_age_rating(p_game_entry, g_age)
+    #Add a GamePrice entry
+    g_price = full_details_json[PSN_JSON_ELEM_GAME_PRICE_BLOCK][PSN_JSON_ELEM_GAME_BASE_PRICE]
+    print("Price: ", g_price)
+    discount_tuple = get_psn_game_discounts(full_details_json[PSN_JSON_ELEM_GAME_PRICE_BLOCK])
+    #g_base_discount = full_details_json[PSN_JSON_ELEM_GAME_PRICE_BLOCK][PSN_JSON_ELEM_GAME_REWARDS][0][PSN_JSON_ELEM_GAME_BASE_DISCOUNT]
+    print("Base Discount: ", discount_tuple.base)
+    #g_plus_discount = full_details_json[PSN_JSON_ELEM_GAME_PRICE_BLOCK][PSN_JSON_ELEM_GAME_REWARDS][0][PSN_JSON_ELEM_GAME_BONUS_DISCOUNT]
+    print("Plus Discount: ", discount_tuple.plus)
+    #add_game_price_entry(p_game_entry, g_price, 10, 20)
+
+def get_psn_game_discounts(p_game_price_block_json):
+    psn_discounts = collections.namedtuple('psn_discounts', ['base', 'plus'])
+    base_discount = 10
+    plus_discount = 10
+    if PSN_JSON_ELEM_GAME_REWARDS in p_game_price_block_json:
+        print("REWARDS!")
+        if PSN_JSON_ELEM_GAME_BASE_DISCOUNT in p_game_price_block_json[PSN_JSON_ELEM_GAME_REWARDS][0]:
+            base_discount = p_game_price_block_json[PSN_JSON_ELEM_GAME_REWARDS][0][PSN_JSON_ELEM_GAME_BASE_DISCOUNT]
+        if PSN_JSON_ELEM_GAME_BONUS_DISCOUNT in p_game_price_block_json[PSN_JSON_ELEM_GAME_REWARDS][0]:
+            plus_discount = p_game_price_block_json[PSN_JSON_ELEM_GAME_REWARDS][0][PSN_JSON_ELEM_GAME_BONUS_DISCOUNT]
+    else:
+        print("NO REWARDS!")
+    return psn_discounts(base=base_discount,plus=plus_discount)
+
+def add_game_price_entry(p_game_entry, p_price, p_base_discount, p_plus_discount):
+    return GamePrice.objects.create(game_id=p_game_entry, last_updated=timezone.now(), base_price=p_price, base_discount=p_base_discount, plus_discount=p_plus_discount)
+
+def set_game_age_rating(p_game_entry, p_new_age_rating):
+    p_game_entry.age_rating = p_new_age_rating
+    p_game_entry.save()
+
+def add_game_list_entry_to_db(p_g_id, p_g_name, p_g_url, p_g_thumb, p_g_age, p_library_obj):
+    return GameList.objects.create(game_id=p_g_id, game_name=p_g_name, json_url=p_g_url, image_url=p_g_thumb, age_rating=p_g_age, library_name=p_library_obj)
+
+def get_psn_thumbnail(p_image_list):
+    game_thumb = None
+    for eachGameImg in p_image_list:
+        if eachGameImg[PSN_JSON_ELEM_GAME_IMGTYPE] == PSN_JSON_ELEM_GAME_IMGTYPE_THUMB:
+            game_thumb = eachGameImg[PSN_JSON_ELEM_GAME_URL]
+            break
+    return game_thumb
+
+def update_psn_game(p_base_game_json):
+    print("Updating game: ", p_base_game_json[PSN_JSON_ELEM_GAME_NAME])
+
+def game_exists_in_db(p_library_id, p_game_id):
+    return (GameList.objects.filter(game_id=p_game_id, library_name=p_library_id).count() != 0)
+
+def game_is_on_PS4(p_platform_list):
+    return PSN_JSON_ELEM_GAME_PS4_PFORM in p_platform_list
+
+def get_psn_lib_json():
+    psn_lib_json = None
+
+    #File for testing only - live version will pull json from psn api
+    with open('staticfiles/psnvalue/TotalPS4GameLibrary.json') as data_file:    
+        psn_lib_json = json.load(data_file)
+
+    return psn_lib_json
+
+def get_psn_game_json(p_game_url):
+    psn_game_json = None
+    #response_json = requests.get(p_game_url)
+    #psn_game_json = response_json.json()
+    #File for testing only - live version will pull json from psn api
+    with open('staticfiles/psnvalue/DragonAgeInquisition_FullGame.json') as data_file:
+    #with open('staticfiles/psnvalue/DarkSoulsIII_FullGame.json') as data_file:
+        psn_game_json = json.load(data_file)
+
+    return psn_game_json
